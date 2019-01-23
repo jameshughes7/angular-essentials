70bb6302324f1b796af145704e71844ab2b4dceb
James Hughes <hugjimbo77@gmail.com>
2019-01-23 22:26:10 +0000

    removed CUSTOM_ELEMENTS_SCHEMA from app.module.ts as it was unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -16,7 +15,6 @@ import { DashboardComponent} from './dashboard.component';
     BrowserModule,
     FormsModule
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
