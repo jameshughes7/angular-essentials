05593de82f52968ab31c6cdfc3701d4a02a0d049
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-01-23 12:05:53 +0000

    first commit: added first custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-essentials';
 }
+
+// @feature is a decorator
+// allows you to attach logic to classes, methods, properties
+// decorator describes construct the follows immediately after it
---
+++ src/app/user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component ({
+  selector: 'app-user',
+  template: `
+    <p>Hello!<p>
+    <p>I'm the user component</p>
+    `
+})
+export class UserComponent {}
