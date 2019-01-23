68fc9a116d2d16bd70acc336c4fd8a103b33199a
James Hughes <hugjimbo77@gmail.com>
2019-01-23 23:00:28 +0000

    user-detail component added, used its selector in another component: use.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
 import { DashboardComponent} from './dashboard.component';
+import { UserDetailComponent} from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
